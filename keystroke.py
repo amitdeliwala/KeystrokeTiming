13e8afbf733f3739694c96ab5bd3a6be294464d4
Amit <amit.deliwala@gmail.com>
2016-08-22 21:22:43 -0400

    Matrix changes

--- keystroke.py
+++ keystroke.py
@@ -1,11 +1,12 @@
 import msvcrt, sys, time
 import numpy as np
 import scipy.io 
-
-mat = [[[0] for x in range(7)] for y in range(7)] 
-matfile = 'test_mat.mat'
-
-letters = ['a','b','c','d','e','f',' '];
+import string
+#mat = [[[0] for x in range(27)] for y in range(27)] 
+matfile = 'test_mat1.mat'
+mat = scipy.io.loadmat(matfile)
+letters = list(string.ascii_lowercase)
+letters.append(' ')
 string = ''
 timing = [];
 xl = chr(ord(msvcrt.getch()))
@@ -18,22 +19,28 @@ for j in range(50):
 
 	x = chr(ord(msvcrt.getch()))
 	t = time.time()
+	if(x == ';'):
+		scipy.io.savemat(matfile, mdict={'out': mat,'stuff': timing}, oned_as='row')
+		break
+
 	i = letters.index(x)
 
-	if(i == 7 or il == 7):
+	if(i == 26 or il == 26):
 		tl = t
 		xl = x
 		il = i
 		print(x)
+
+
 	else:
 		string.join(str(x))
 		print(x)
+		#mat['out'][i][il] = np.insert(mat['out'][i][il],0,t-tl)
 		mat[i][il].append(t-tl)
 		timing.append(t-tl)
 		tl = t
 		xl = x
 		il = i
-
 	
 	
 
@@ -41,4 +48,3 @@ for j in range(50):
 
 print(timing)
 
-scipy.io.savemat(matfile, mdict={'out': mat,'stuff': timing}, oned_as='row')
